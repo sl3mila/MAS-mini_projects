cd6bead880c09cf7cc70014670f3c24cddabc6fd
sl3mila <s28796@gmail.com>
2025-05-19 12:15:03 +0200

    mini project 5 prepared

---
+++ s28796_MP5/src/main/java/pja/mas/eslemp/s28796_mp5/S28796Mp5Application.java
@@ -0,0 +1,13 @@
+package pja.mas.eslemp.s28796_mp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S28796Mp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S28796Mp5Application.class, args);
+    }
+
+}
---
+++ s28796_MP5/src/test/java/pja/mas/eslemp/s28796_mp5/S28796Mp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pja.mas.eslemp.s28796_mp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S28796Mp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
